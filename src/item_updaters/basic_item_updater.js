6d83298e5018b0138b13bbb82277efd2cbf36eb5
Lyr-7D1h <lyr-7d1h@pm.me>
2020-12-19 14:09:37 +0100

    Added jsdocs for basicItemUpdater

--- src/item_updaters/basic_item_updater.js
+++ src/item_updaters/basic_item_updater.js
@@ -1,13 +1,19 @@
 class BasicItemUpdater {
+  /**
+   * Create an object that handles the update logic of an Item
+   * @param {object} item An object with quality, name and sellIn properties
+   */
   constructor(item) {
     this.item = item;
   }
 
+  /** Update item */
   update() {
     this.updateQuality();
     this.item.sellIn -= 1;
   }
 
+  /** Update Quality of item */
   updateQuality() {
     const newQuality = this.getNewQuality();
     if (newQuality > 50) return (this.item.quality = 50);
@@ -15,10 +21,12 @@ class BasicItemUpdater {
     this.item.quality = newQuality;
   }
 
+  /** Calculate the new quality returns a number */
   getNewQuality() {
     return this.item.quality + 1 * this.getUpdateRatio();
   }
 
+  /** Returns a value by how much the quality should be updated */
   getUpdateRatio() {
     if (this.item.sellIn <= 0) return -2;
     return -1;
